be0690f70f09abba9f05675291443c15a5db2831
Maximchicus <da.riel@yandex.ru>
2019-05-28 21:04:12 +0300

    monsterband

--- Assets/Scripts/Characters/Enemy.cs
+++ Assets/Scripts/Characters/Enemy.cs
@@ -1,33 +1,45 @@
-﻿using Data;
+﻿using System;
+using Data;
+using Game;
 using UnityEngine;
+using Random = System.Random;
 
 namespace Characters
 {
     public class Enemy : MonoBehaviour
     {
+        public GameInteraction gameInteraction;
+        public Data.Data data;
         public int damage;
         public int health;
         public int maxHp;
         public int typeOfDamage;    // 1 - магический. 2 - физический
         public int magicResist;
         public int physicalResist;
+        private Random rnd = new Random((int)DateTime.Now.Ticks);
+
+        private void Update()
+        {
+            rnd = new Random((int)DateTime.Now.Ticks);
+        }
+
         public void CreateEnemy()
         {
-            health = 95 + DataInteraction.GetHero(Random.Range(1, 3)).level * 5;
+            health = 45 + DataInteraction.GetHero(rnd.Next(1, 3)).level * rnd.Next(5, 20);
             maxHp = health;
-            damage = 15 + DataInteraction.GetHero(Random.Range(1, 3)).level * 5;
-            typeOfDamage = Random.Range(1, 2);
-            switch (Random.Range(1, 3))
+            damage = 15 + DataInteraction.GetHero(rnd.Next(1, 3)).level * rnd.Next(1, 6);
+            typeOfDamage = rnd.Next(1, 2);
+            switch (rnd.Next(1, 3))
             {
                 case 1:
-                    magicResist = Random.Range(0, DataInteraction.GetHero(Random.Range(1, 3)).level * 3);
+                    magicResist = rnd.Next(0, DataInteraction.GetHero(rnd.Next(1, 3)).level * 4);
                     break;
                 case 2:
-                    physicalResist = Random.Range(0, DataInteraction.GetHero(Random.Range(1, 3)).level * 3);
+                    physicalResist = rnd.Next(0, DataInteraction.GetHero(rnd.Next(1, 3)).level * 4);
                     break;
                 case 3:
-                    magicResist = Random.Range(0, DataInteraction.GetHero(Random.Range(1, 3)).level * 3);
-                    physicalResist = Random.Range(0, DataInteraction.GetHero(Random.Range(1, 3)).level * 3);
+                    magicResist = rnd.Next(0, DataInteraction.GetHero(rnd.Next(1, 3)).level * 4);
+                    physicalResist = rnd.Next(0, DataInteraction.GetHero(rnd.Next(1, 3)).level * 4);
                     break;
             }
         }
---
+++ Assets/Scripts/Game/MonsterBand.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Characters;
+using UnityEngine;
+
+public class MonsterBand : MonoBehaviour
+{
+    public Enemy monster1;
+    public Enemy monster2;
+    public Enemy monster3;
+
+    public void Start()
+    {
+        monster1.CreateEnemy();
+        monster2.CreateEnemy();
+        monster3.CreateEnemy();
+    }
+}
