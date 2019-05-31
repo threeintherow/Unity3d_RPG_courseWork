b0703fbadfb1d8c485ba0acab6377ddca2560473
Leshka <31736716+TOPlessss@users.noreply.github.com>
2019-05-31 19:21:44 +0300

    panel

---
+++ Assets/Scripts/Game/PanelOpener.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PanelOpener : MonoBehaviour
+{
+    public GameObject monsterPanel;
+
+    public void OnMouseDown()
+    {
+        if (monsterPanel != null)
+        {
+            bool isActive = monsterPanel.activeSelf;
+            monsterPanel.SetActive(!isActive);
+        }
+    }
+}
